3433857d731e8b2d3ebd90acc8d504dfca935553
Katya <kate0706@mail.ru>
2018-04-22 14:00:56 +0200

    Added Test project

---
+++ CodingTasks/CodingTasks.Tests/Arrays/Add1ToNumberTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using CodingTasks.Arrays;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CodingTasks.Tests.Arrays
+{
+    [TestFixture]
+    public class Add1ToNumberTests
+    {
+        [TestCase(new [] { 0 }, new [] { 1 })]
+        [TestCase(new[] { 1, 1, 1, 3, 2, 1, 1, 2, 5, 9, 6, 5 }, new[] { 1, 1, 1, 3, 2, 1, 1, 2, 5, 9, 6, 6 })]
+        [TestCase(new[] { 1, 2, 4 }, new[] { 1, 2, 5 })]
+        [TestCase(new[] { 9, 9, 9, 9, 9 }, new[] { 1, 0, 0, 0, 0, 0 })]
+        [TestCase(new[] { 0, 0, 4, 5, 6 }, new[] { 4, 5, 7 })]
+        [TestCase(new[] { 0, 0, 9, 9, 9 }, new[] { 1, 0, 0, 0 })]
+        public void Should_Pass_Cases(int[] input, int[] expected)
+        {
+            // Arrange
+            var obj = new Add1ToNumber();
+
+            // Act
+            var actual = obj.plusOne(input.ToList());
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected.ToList());
+        }
+    }
+}
---
+++ CodingTasks/CodingTasks/Arrays/Add1ToNumber.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTasks.Arrays
+{
+    public class Add1ToNumber
+    {
+        public List<int> plusOne(List<int> input)
+        {
+            var j = 0;
+            var A = new List<int>();
+            if (input.Count > 1)
+            {
+                while (input[j] == 0)
+                {
+                    j++;
+                }
+            }
+            for (var i = j; i < input.Count; i++)
+            {
+                A.Add(input[i]);
+            }
+
+            var transferable = false;
+            for (var i = A.Count - 1; i >= 0; i--)
+            {
+                if (transferable)
+                {
+                    if (A[i] < 9)
+                    {
+                        transferable = false;
+                        A[i]++;
+                        break;
+                    }
+
+                    A[i] = 0;
+                }
+                else
+                {
+                    if (A[i] < 9)
+                    {
+                        A[i]++;
+                        break;
+                    }
+
+                    A[i] = 0;
+                    transferable = true;
+                }
+            }
+
+            var output = new List<int>();
+            if (transferable)
+            {
+                output.Add(1);
+            }
+
+            output.AddRange(A);
+
+            return output;
+        }
+    }
+}
