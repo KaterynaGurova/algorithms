0813c387ff665ae5eb6f1d8ab716f17f4b413589
Katya <kate0706@mail.ru>
2018-04-22 21:12:42 +0200

    + Max non-negative subarray

---
+++ CodingTasks/CodingTasks.Tests/Arrays/MaxNonNegativeSubArrayTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CodingTasks.Arrays;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CodingTasks.Tests.Arrays
+{
+    [TestFixture]
+    public class MaxNonNegativeSubArrayTests
+    {
+        [TestCase(new []{ 336465782, -278722862, -2145174067, 1101513929, 1315634022,
+            -1369133069, 1059961393, 628175011, -1131176229, -859484421},
+            new []{ 1101513929, 1315634022})]
+        [TestCase(new[] { 756898537, -1973594324, -2038664370, -184803526, 1424268980 }, new[] { 1424268980 })]
+        public void Should_Pass_Cases(int[] input, int[] expected)
+        {
+            // Arrange
+            var obj = new MaxNonNegativeSubArray();
+
+            // Act
+            var actual = obj.maxset(input.ToList());
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected.ToList());
+        }
+    }
+}
---
+++ CodingTasks/CodingTasks/Arrays/MaxNonNegativeSubArray.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingTasks.Arrays
+{
+    public class MaxNonNegativeSubArray
+    {
+        public List<int> maxset(List<int> A)
+        {
+            long currMax = long.MinValue;
+            List<int> currMaxArr = new List<int>();
+            var currMaxItemsCount = 0;
+            long tempSum = 0;
+            var tempArr = new List<int>();
+            var tempItemsCount = 0;
+            for (var i = 0; i < A.Count; i++)
+            {
+                if (A[i] < 0)
+                {
+                    if (tempItemsCount == 0)
+                    {
+                        continue;
+                    }
+
+                    if (tempSum > currMax
+                        || (tempSum == currMax && currMaxItemsCount <= tempItemsCount))
+                    {
+                        currMax = tempSum;
+                        currMaxArr = tempArr;
+                        currMaxItemsCount = tempItemsCount;
+                    }
+
+                    tempItemsCount = 0;
+                    tempArr = new List<int>();
+                    tempSum = 0;
+                }
+                else
+                {
+                    tempSum += A[i];
+                    tempArr.Add(A[i]);
+                    tempItemsCount++;
+                }
+            }
+
+            if (tempItemsCount > 0)
+            {
+                if (tempSum > currMax
+                       || (tempSum == currMax && currMaxItemsCount <= tempItemsCount))
+                {
+                    currMaxArr = tempArr;
+                }
+            }
+
+            return currMaxArr;
+        }
+    }
+}
