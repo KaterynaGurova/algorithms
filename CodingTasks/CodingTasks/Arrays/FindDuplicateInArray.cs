a265bcda02cb6e925a1c208a5203d5c2a78b73e7
Katya <kate0706@mail.ru>
2018-04-23 15:45:46 +0200

    Added working FindDuplicateInArry, and not working MaxDistance

---
+++ CodingTasks/CodingTasks/Arrays/FindDuplicateInArray.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace CodingTasks.Arrays
+{
+    public class FindDuplicateInArray
+    {
+        public int repeatedNumber(List<int> A)
+        {
+            var existing = new Dictionary<int, int>();
+            for (var i = 0; i < A.Count; i++)
+            {
+                if (!existing.ContainsKey(A[i]))
+                {
+                    existing[A[i]] = 0;
+                }
+
+                existing[A[i]]++;
+            }
+
+            A.Sort();
+
+            var max = 0;
+            var maxNumber = -1;
+            foreach (var i in existing)
+            {
+                if (i.Value >= max)
+                {
+                    max = i.Value;
+                    maxNumber = i.Key;
+                }
+            }
+
+            return maxNumber;
+        }
+    }
+}
---
+++ CodingTasks/CodingTasks/Arrays/MaxDistance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTasks.Arrays
+{
+    public class MaxDistance
+    {
+        public int maximumGap(List<int> A)
+        {
+            var currMaxDist = 0;
+            var maxDist = -1;
+            if (A.Count == 1)
+            {
+                return 0;
+            }
+            A.Sort();
+
+
+                for (var j = 0; j < A.Count - 1; j++)
+                {
+                    if (A[j] <= A[j + 1])
+                    {
+                        currMaxDist++;
+                    }
+                    else
+                    {
+                        currMaxDist = 0;
+                    }
+
+                    if (currMaxDist > maxDist)
+                    {
+                        maxDist = currMaxDist;
+                    }
+                }
+
+
+            return maxDist;
+        }
+    }
+}
