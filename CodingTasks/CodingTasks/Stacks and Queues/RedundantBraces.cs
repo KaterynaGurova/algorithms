895d2809e5053389da6f0e77df3847a87dd2054e
Katya <kate0706@mail.ru>
2018-05-01 14:31:59 +0200

    1st of May commit miss

---
+++ CodingTasks/CodingTasks/Stacks and Queues/RedundantBraces.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace CodingTasks
+{
+    public class RedundantBraces
+    {
+        public int braces(string A)
+        {
+            var stack = new Stack<char>();
+            var i = 0;
+            while (i < A.Length)
+            {
+                while (i < A.Length && A[i] != ')')
+                {
+                    stack.Push(A[i]);
+                    i++;
+                }
+                if (i >= A.Length) return 0;
+                i++;
+                var j = 0;
+                while (stack.Count > 0)
+                {
+                    var item = stack.Pop();
+                    if (item == '(')
+                        break;
+                    j++;
+                }
+
+                if (j < 3) return 1;
+
+                stack.Push('a');
+            }
+
+            return 0;
+        }
+    }
+}
